2b38bc1dd445e7be93f50df0aec7508ca96600b5
Rebecca-zy <273146618@qq.com>
2021-01-19 15:13:55 +0800

    更新test

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
+    <router-link to="pethome">topethome</router-link>
+    <router-link to="search">tosearch</router-link>
+    <router-link to="knowledgecard">toknowledgecard</router-link>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+const axios=require('axios');
 export default {
   name:'App'
 }
@@ -13,5 +17,11 @@ export default {
 <style>
 
 .el-table--border th.gutter:last-of-type { display: block!important; width: 17px!important; }
-
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,21 @@ import Othershome from '../views/othershome'
 import PetRegister from '../components/petregister'
 import PetLog from '../components/petlog'
 import Home from '../components/home'
+import Content from "../components/Content";
+import Login from "../views/Login";
+import Main from "../views/Main";
+import Testcomp from "../components/Testcomp/Testcomp";
+import like from "../components/like";
+import collect from "../components/collect";
+import user from "../components/user";
+import section from "../components/section";
+import rank from "../components/rank";
+import sortcomp from "../components/sortcomp/sortcomp";
+import register from "../components/register";
+import search from "../components/search";
+import pethome from "../components/pethome";
+import lan from "../components/lan"
+import knowledgecard from "../components/knowledgecard";
 Vue.use(Router)
 
 export default  new Router({
@@ -34,6 +49,105 @@ export default  new Router({
       path:'/home',
       name:'Home',
       component:Home
-    }
+    },
+    {
+      //路由路径
+      path: '/content',
+      //跳转的组件
+      name: 'content',
+      component: Content,
+    },
+    {
+      path:'/login',
+      name:'login',
+      component: Login,
+      meta: {
+        requireAuth: true,  // 添加该字段，表示进入这个路由是需要登录的
+      },
+    },
+    {
+      path:'/main',
+      name:'Main',
+
+      component: Main,
+      children:[
+        {
+          path:'/like',
+          name:'like',
+          component: like
+        },
+
+        {
+          path:'/collect',
+          name:'collect',
+          component: collect
+        },
+        {
+          path:'/user',
+          name:'user',
+          component: user
+        },
+        {
+          path:'/section',
+          name:'section',
+          component: section,
+          meta: {
+            requireAuth: true,  // 添加该字段，表示进入这个路由是需要登录的
+          },
+        },
+        {
+          path:'/rank',
+          name:'rank',
+          component: rank
+        },
+
+      ]
+    },
+    {
+      path:'/test',
+      name:'test',
+      component: Testcomp
+    },
+    {
+      path:'/sort',
+      name:'sort',
+      component: sortcomp
+    },
+    {
+      //路由路径
+      path: '/register',
+      //跳转的组件
+      name: 'register',
+      component: register,
+    },
+    {
+      //路由路径
+      path: '/search',
+      //跳转的组件
+      name: 'search',
+      component: search,
+
+    },
+    {
+      //路由路径
+      path: '/pethome',
+      //跳转的组件
+      name: 'pethome',
+      component: pethome
+    },
+    {
+      //路由路径
+      path: '/lan',
+      //跳转的组件
+      name: 'lan',
+      component: lan
+    },
+    {
+      //路由路径
+      path: '/knowledgecard',
+      //跳转的组件
+      name: 'knowledgecard',
+      component: knowledgecard
+    },
   ]
 })
