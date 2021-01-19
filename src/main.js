9e6df4c47c1564f34662d27f0203e3299a904ad1
nnnnullll <1454672993@qq.com>
2021-01-19 10:54:31 +0800

    10：54

--- src/main.js
+++ src/main.js
@@ -7,18 +7,8 @@ import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 Vue.prototype.$axios = axios;
-
-
-
-// //引入vue2-svg-icon并且注册组件
-// import Icon from 'vue2-svg-icon/Icon.vue'
-// Vue.component('icon',Icon);
-
-
 Vue.config.productionTip = false
 Vue.use(ElementUI);
-
-
 
 
 new Vue({
