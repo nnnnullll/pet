20674149967405267b40c956d90c8a75e160ef3a
Rebecca-zy <273146618@qq.com>
2021-01-25 16:21:54 +0800

    更新了知识卡片前端页面

--- src/components/knowledgecard.vue
+++ src/components/knowledgecard.vue
@@ -1,12 +1,26 @@
-// 宠儿识别
-<template>
+// 宠物识别
+<template xmlns:android="http://www.w3.org/1999/xhtml">
   <div class="building">
     <!-- 首栏 -->
     <v-top></v-top>
     <!-- 识别 -->
     <div class="bottom">
-      <div style="display: flex;flex-direction: column">
+      <div class="information" style="display: flex;flex-direction: column;">
+        <div style="display: flex;flex-direction: row;justify-content: center;">
+          <img :src="love" class="love1">
+          <p class="title">你想了解的我</p>
+          <img :src="love" class="love2">
+        </div>
+        <p class="pz">我的品种名字：{{pz}}</p>
+        <p class="bm">别名：{{bm}}</p>
+        <p class="zl">种类：{{zl}}</p>
+        <p class="tx">体型：{{tx}}</p>
+        <p class="xgtz">性格特征：{{xgtz}}</p>
+        <p class="jj">简介：{{jj}}</p>
+      </div>
+      <div style="display: flex;flex-direction: column;">
         <img :src="flags" class="flags">
+        <button class="petbtn">👣宠物识别</button>
         <img :src="pet" class="pet">
         <img :src="border" class="border">
       </div>
@@ -28,7 +42,21 @@ export default {
       },
       flags:require("@/assets/img/flags.png"),
       border:require("@/assets/img/border.png"),
-      pet:require('@/assets/img/userimg.png')
+      pet:require('@/assets/img/userimg.png'),
+      love:require('@/assets/img/love.png'),
+      pz:"心肝小宝贝喵喵",
+      bm:"囡囡",
+      zl:"团宠喵",
+      tx:"小巧",
+      xgtz:"温顺可爱、友善机灵",
+      jj:"喵喵是我们网页设计团队的心头宝，几乎一眼间\n" +
+          "就抓到了团队成员的心。它是我们的团宠喵，它也叫囡\n" +
+          "囡。它有耐心且拥有健美形态，拥有定期运动意识，饲\n" +
+          "喂食物和兽医体检就可以，非常令人省心。属于匀称、\n" +
+          "有力、活泼的喵星人，\n" +
+          "喵的身体各部位配合合理，腿既不太长也不笨拙，表情\n" +
+          "友善，个性热情、机警、自信而且不怕生，性格讨人喜\n" +
+          "欢。",
     }
   },
 }
@@ -57,10 +85,92 @@ body {
   margin-left: 30%;
 }
 .pet{
-  width: 259px;
   height: 348px;
   position: absolute;
   margin-top: 25%;
   margin-left: 10%;
+}
+.petbtn{
+  width: 294px;
+  height: 55px;
+  background: #308DC2;
+  opacity: 0.9;
+  border-radius: 10px;
+  position: absolute;
+  margin-top: 18%;
+  margin-left: 11.5%;
+  font-size: 30px;
+  font-family: ZTSJ-BaguetteFont;
+  font-weight: 400;
+  color: #FFFFFF;
+}
+.love1{
+  width: 40px;
+  height: 42px;
+}
+.love2{
+  width: 40px;
+  height: 42px;
+}
+.information{
+  position: absolute;
+  margin-left: 55%;
+  margin-top: 20%;
+}
+.pz{
+  width: 513px;
+  height: 21px;
+  font-size: 21px;
+  font-family: ZTSJ-BaguetteFont;
+  font-weight: 400;
+  color: #737373;
+}
+.bm{
+  width: 513px;
+  height: 20px;
+  font-size: 21px;
+  font-family: ZTSJ-BaguetteFont;
+  font-weight: 400;
+  color: #737373;
+}
+.zl{
+  width: 513px;
+  height: 21px;
+  font-size: 21px;
+  font-family: ZTSJ-BaguetteFont;
+  font-weight: 400;
+  color: #737373;
+}
+.tx{
+  width: 513px;
+  height: 20px;
+  font-size: 21px;
+  font-family: ZTSJ-BaguetteFont;
+  font-weight: 400;
+  color: #737373;
+}
+.xgtz{
+  width: 513px;
+  height: 21px;
+  font-size: 21px;
+  font-family: ZTSJ-BaguetteFont;
+  font-weight: 400;
+  color: #737373;
+}
+.jj{
+  width: 513px;
+  height: 283px;
+  font-size: 21px;
+  font-family: ZTSJ-BaguetteFont;
+  font-weight: 400;
+  color: #737373;
+}
+.title{
+  width: 126px;
+  height: 21px;
+  font-size: 21px;
+  font-family: ZTSJ-BaguetteFont;
+  font-weight: 400;
+  color: #737373;
 }
 </style>
